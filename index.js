f51068a55ebe6394607eab2b11cf12e4540ce63f
Alberto Castro <Alberto1973@live.com>
2022-08-07 00:15:43 -0400

    commit tree 5bccc0d2581a8145df910844947d0f25a9aba951

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    if (blockNumber > 42) {
+        return blockNumber - 42
+     } else if ( blockNumber < 42 ) {
+        return 42 - blockNumber
+    }
+}
+function distanceFromHqInFeet(blockNumber) {
+   return distanceFromHqInBlocks(blockNumber) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if(start > destination) {
+    return (start - destination) * 264
+  }
+  else {
+    return (destination - start) * 264
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+    if(distanceTravelledInFeet(start, destination) * 264 <= 400) {
+        return 0
+    }
+    else if (400 < distanceTravelledInFeet((start, destination) * 264) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02
+    }
+}
